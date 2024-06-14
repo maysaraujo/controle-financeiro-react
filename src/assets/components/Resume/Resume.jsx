dc96bff7ff84a2f97c07fc99669101c2878aa8db
maysaraujo <maysaaraujo25@gmail.com>
2024-06-14 10:53:37 -0300

    feat: creating Resume component

---
+++ src/assets/components/Resume/Resume.jsx
@@ -0,0 +1,19 @@
+import ResumeItem from '../ResumeItem/ResumeItem';
+import * as C from './style';
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from 'react-icons/fa';
+
+const Resume = () => {
+  return (
+    <C.Container>
+      <ResumeItem title='Entradas' Icon={FaRegArrowAltCircleUp} value='1000' />
+      <ResumeItem title='Saídas' Icon={FaRegArrowAltCircleDown} value='1000' />
+      <ResumeItem title='Total' Icon={FaDollarSign} value='1000' />
+    </C.Container>
+  );
+};
+
+export default Resume;
