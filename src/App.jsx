e8b134215a570886619e8c75061561358242131b
maysaraujo <maysaaraujo25@gmail.com>
2024-06-14 10:52:26 -0300

    feat: adding Header, Resume, Form and GlobalStyle components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Form from './assets/components/Form/Form';
+import Header from './assets/components/Header/Header';
+import Resume from './assets/components/Resume/Resume';
+import GlobalStyle from './styles/globals';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Resume />
+      <Form />
+      <GlobalStyle />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
