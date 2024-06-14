964263db57b53e146bba01afe7d061f254f17d65
maysaraujo <maysaaraujo25@gmail.com>
2024-06-14 10:54:57 -0300

    feat: creating Global styles

---
+++ src/styles/globals.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif;
+    background-color: #F2F2F2;
+  }
+`;
+
+export default Global;
