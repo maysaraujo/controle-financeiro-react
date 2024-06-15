f037bedb914a694c662a5f8c456ec13e085002e9
maysaraujo <maysaaraujo25@gmail.com>
2024-06-15 19:45:43 -0300

    feat: updating Form component

--- src/assets/components/Form/Form.jsx
+++ src/assets/components/Form/Form.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import * as C from './style';
+import Grid from '../Grid/Grid';
 
-const Form = () => {
+const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
   const [desc, setDesc] = useState('');
   const [amount, setAmount] = useState('');
   const [isExpense, setIsExpense] = useState(false);
+
+  const generateID = () => Math.round(Math.random() * 1000);
 
   const handleSave = () => {
     if (!desc || !amount) {
@@ -14,44 +17,59 @@ const Form = () => {
       alert('O valor tem que ser positivo!');
       return;
     }
+
+    const transaction = {
+      id: generateID(),
+      desc: desc,
+      amount: amount,
+      expense: isExpense,
+    };
+
+    handleAdd(transaction);
+
+    setDesc('');
+    setAmount('');
   };
 
   return (
-    <C.Container>
-      <C.InputContent>
-        <C.Label>Descrição</C.Label>
-        <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
-      </C.InputContent>
+    <>
+      <C.Container>
+        <C.InputContent>
+          <C.Label>Descrição</C.Label>
+          <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+        </C.InputContent>
 
-      <C.InputContent>
-        <C.Label>Valor</C.Label>
-        <C.Input
-          value={amount}
-          type='number'
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </C.InputContent>
+        <C.InputContent>
+          <C.Label>Valor</C.Label>
+          <C.Input
+            value={amount}
+            type='number'
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </C.InputContent>
 
-      <C.RadioGroup>
-        <C.Input
-          type='radio'
-          id='rIncome'
-          defaultChecked
-          name='group1'
-          onChange={() => setIsExpense(!isExpense)}
-        />
-        <C.Label htmlFor='rIncome'>Entrada</C.Label>
+        <C.RadioGroup>
+          <C.Input
+            type='radio'
+            id='rIncome'
+            defaultChecked
+            name='group1'
+            onChange={() => setIsExpense(!isExpense)}
+          />
+          <C.Label htmlFor='rIncome'>Entrada</C.Label>
 
-        <C.Input
-          type='radio'
-          id='rExpenses'
-          name='group1'
-          onChange={() => setIsExpense(!isExpense)}
-        />
-        <C.Label htmlFor='rExpenses'>Saída</C.Label>
-      </C.RadioGroup>
-      <C.Button onClick={handleSave}>Adicionar</C.Button>
-    </C.Container>
+          <C.Input
+            type='radio'
+            id='rExpenses'
+            name='group1'
+            onChange={() => setIsExpense(!isExpense)}
+          />
+          <C.Label htmlFor='rExpenses'>Saída</C.Label>
+        </C.RadioGroup>
+        <C.Button onClick={handleSave}>Adicionar</C.Button>
+      </C.Container>
+      <Grid itens={transactionsList} setItens={setTransactionsList} />
+    </>
   );
 };
 
