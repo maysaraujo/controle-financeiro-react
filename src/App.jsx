4492cd51591b981cabbd70dba6c729be525b7686
maysaraujo <maysaaraujo25@gmail.com>
2024-06-15 19:45:34 -0300

    feat: updating App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
+import { useEffect, useState } from 'react';
 import Form from './assets/components/Form/Form';
 import Header from './assets/components/Header/Header';
 import Resume from './assets/components/Resume/Resume';
 import GlobalStyle from './styles/globals';
 
 function App() {
+  const data = localStorage.getItem('transactions');
+  const [transactionsList, setTransactionsList] = useState(
+    data ? JSON.parse(data) : []
+  );
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transaction) => Number(transaction.amount));
+
+    const amountIncome = transactionsList
+      .filter((item) => !item.expense)
+      .map((transaction) => Number(transaction.amount));
+
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+    const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+    const total = Math.abs(income - expense).toFixed(2);
+
+    setIncome(`R$ ${income}`);
+    setExpense(`R$ ${expense}`);
+    setTotal(`${Number(income) < Number(expense) ? '-' : ''}R$ ${total}`);
+  }, [transactionsList]);
+
+  const handleAdd = (transaction) => {
+    const newArrayTransactions = [...transactionsList, transaction];
+
+    setTransactionsList(newArrayTransactions);
+
+    localStorage.setItem('transactions', JSON.stringify(newArrayTransactions));
+  };
+
   return (
     <>
       <Header />
-      <Resume />
-      <Form />
+      <Resume income={income} expense={expense} total={total} />
+      <Form
+        handleAdd={handleAdd}
+        transactionsList={transactionsList}
+        setTransactionsList={setTransactionsList}
+      />
       <GlobalStyle />
     </>
   );
